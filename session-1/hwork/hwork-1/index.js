00cd11eac73ed9963acc89841dee008b0be91d39
insert github real name here <insert github email here>
2020-10-13 20:53:07 +0700

    bai tap

---
+++ session-1/hwork/hwork-1/index.js
@@ -0,0 +1,17 @@
+console.log("bài 1: ");
+console.log("A. trong JS, xảy ra lỗi SyntaxError khi: ");
+console.log("1. Biến được khởi tạo bắt đầu bằng số. ");
+console.log("2. Biến được khởi tạo chứa nhưng ký tự đặc biệt. ");
+console.log("3. Biến được khởi tạo trùng tên với các từ khóa. ");
+console.log("\n");
+console.log("Ví dụ: ");
+console.log("VD1: Biến được khởi tạo bắt đầu bằng số let 1a. ");
+console.log("VD2: Biến được khởi tạo chứa nhưng ký tự đặc biệt 123@. ");
+console.log("VD3: Biến được khởi tạo trùng tên với các từ khóa let break. ");
+
+console.log("\n");
+
+console.log("B. Trong JS, để kiểm tra một dữ liệu biến: ");
+console.log("Dùng lệnh typeof để xem kiểu dữ liệu của biến. ");
+let name = 'Nguyễn Tiến Lâm';
+console.log('VD: let name = Nguyễn Tiến Lâm' + typeof(name) );
